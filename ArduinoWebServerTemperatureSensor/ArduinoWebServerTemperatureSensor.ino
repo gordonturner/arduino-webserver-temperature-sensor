0fec227ad1c1b7cc904bd543e5fc6deb3550d4a3
Gordon Turner <gordon.turner@gmail.com>
2013-03-13 19:15:04 -0400

    Adding libraries and changes to comments.

--- ArduinoWebServerTemperatureSensor/ArduinoWebServerTemperatureSensor.ino
+++ ArduinoWebServerTemperatureSensor/ArduinoWebServerTemperatureSensor.ino
@@ -1,14 +1,19 @@
 /*
   Arduino Web Server Temperature Sensor
-  
-  - Adapted by Gordon Turner from code by David A. Mellis and Tom Igoe.
  
   A web server that will read the values of a 1-Wire temperature sensor and output 
   XML with temperature in Celcius and Fahrenheit.
+  
+  - Adapted by Gordon Turner from code by David A. Mellis and Tom Igoe.
  
   - Ethernet shield attached to pins 10, 11, 12, 13
   - 1-Wire temperature sensor attached to digital pin 3.
- 
+   
+  - Requires 1-wire Ardunio Library and Dallas Temperature Control Arduino Library.
+  - Included in Libraries folder or online:
+      http://www.pjrc.com/teensy/arduino_libraries/OneWire.zip
+      http://milesburton.com/Dallas_Temperature_Control_Library#Latest
+      
   - Sample output XML:
  
   <?xml version=\"1.0\"?>
